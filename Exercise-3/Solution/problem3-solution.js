6da5409416e1d145a15de7debda5cf04525f52f8
davidshonia <dupecube1@gmail.com>
2021-11-01 21:01:47 +0400

    callback

--- Exercise-3/Solution/problem3-solution.js
+++ Exercise-3/Solution/problem3-solution.js
@@ -13,18 +13,18 @@ function jetMarket(itemNum){
 function buyNewJet(jet, callback){
   setTimeout(function(){
     ourJetDB.push(jet);
+    console.log('Jet Bought Successfully')
     callback();
-  }, 3000);
+  }, 1500);
 }
 
 function getOwnList(){
   setTimeout(function(){
-      console.log('We Bought: \n'+ourJetDB);
-  }, 1000)
+      console.log('We Got: \n'+ourJetDB);
+  }, 3000)
 }
 
 
-buyNewJet(jetMarket(1), getOwnList);
 buyNewJet(jetMarket(0), getOwnList);
 jetMarket();
 
